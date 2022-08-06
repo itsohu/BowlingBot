227788539b9d34573e8e1ed356e99d38374fc43d
itsohu <110700866+itsohu@users.noreply.github.com>
2022-08-06 01:16:54 -0300

    Update bowlingBot.py

--- bowlingBot.py
+++ bowlingBot.py
@@ -3,6 +3,6 @@ import os
 import time
 path=os.getcwd()
 #The above line gets the path so I can give it to the locate on screen function
-path=path+"\\bowlingball.PNG"
-Boxcoord=time.sleep(1);pgui.locateOnScreen(path)
+path=path+"\\bowlingball2.PNG"
+time.sleep(5);Boxcoord=pgui.locateOnScreen(path)
 coord=pgui.center(Boxcoord)
